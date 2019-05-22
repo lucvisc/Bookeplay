e1d69ac7a3dbc107c1f4a54e2245f2f799f08b8f
lucvisc <lucavisconti97@gmail.com>
2019-05-22 11:26:50 +0200

    creazione delle classi Foundation

---
+++ Foundation/FDatabase.php
@@ -0,0 +1,293 @@
+<?php
+
+
+//require_once 'include.php';
+
+if (file_exists('config.inc.php')) require_once 'config.inc.php';
+/*  Singleton: rappresenta un tipo particolare di classe che garantisce
+ *  che soltanto un'unica istanza della classe stessa possa essere creata
+ *  all'interno di un programma
+ */
+
+/**
+ * Lo scopo di questa classe e' quello di fornire un accesso unico al DBMS, cosi che l'accesso
+ * ai dati persistenti da parte degli strati superiore dell'applicazione sia piu' intuitivo.
+ *
+ * @author Luca, Catriel
+ * @package Foundation
+ */
+class FDatabase
+{
+
+    private static $instance = null;     /** l'unica istanza della classe */
+
+    private $db;        /** oggetto PDO che effettua la connessione al dbms */
+
+    private static $UpPath = "Upload/";     /** percorso */
+
+    /** dichiarazione del costruttore privato, l'unico accesso è dato dal metodo getInstance() */
+    private function __construct()
+    {
+        global $host, $bookeplay, $username, $password;
+        try {
+            $this->db = new PDO("mysql:host=$host; dbname=$bookeplay", $username, $password);
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            die;
+        }
+    }
+
+    /**
+     * Metodo che restituisce l'unica istanza dell'oggetto.
+     * @return FPersistantManager l'istanza dell'oggetto.
+     */
+    public static function getInstance()    //restituisce l'unica istanza, l'istanza viene creata se non esiste
+    {
+        if (static::$instance == null) {
+            static::$instance = new FDatabase();
+        }
+        return static::$instance;
+    }
+
+    /**
+     * Metodo che permette di salvare le informazioni contenute in un oggetto
+     * delle classe Entity sul database.
+     */
+    public function store($sql, $class, $eobj)
+    {
+        try {
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare($sql);
+            $class::bind($stmt, $eobj);
+            $stmt->execute();
+            $id = $this->db->lastInsertId();
+            $this->db->commit();
+            $this->closeDbConnection();
+            return $id;
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            $this->db->rollBack();
+            die;
+            return null;
+        }
+    }
+
+    /**
+     * Funzione che viene utilizzata per la load quando ci si aspetta che la query produca più di un risultato
+     * da salvare all'interno del database
+     * @param $sql query da eseguire
+     */
+    public function loadMultiple($sql)
+    {
+        try {
+            $rows = array();
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            while ($row = $stmt->fetch()) {
+                $rows[] = $row;
+            }
+            $this->closeDbConnection();
+            return $rows;
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            die;
+            return null;
+        }
+    }
+
+    /**
+     * Funzione che viene utilizzata per la load quando ci si aspetta che la query produca un solo risultato
+     * per esempio load tramite un parametro ID
+     * @param $sql query da eseguire
+     */
+    public function loadSingle($sql)
+    {
+        try {
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->closeDbConnection();
+            return $row;
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            die;
+            return null;
+        }
+    }
+
+    /**
+     * Funzione che viene utilizzata per eliminare una riga quando si passa un unico paramentro (per esempio ID)
+     * @param $sql query da eseguire
+     */
+    public function delete($sql)
+    {
+        try {
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $this->db->commit();
+            $this->closeDbConnection();
+            return true;
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            $this->db->rollBack();
+            die;
+            return false;
+        }
+    }
+
+    /**
+     * Funzione che viene utilizzata per aggiornare una riga quando si passa un unico paramentro (per esempio ID)
+     * @param $sql query da eseguire
+     */
+    public function update($sql)
+    {
+        try {
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $this->db->commit();
+            $this->closeDbConnection();
+            return true;
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            $this->db->rollBack();
+            die;
+            return false;
+        }
+    }
+    /**
+     * Funzione che viene utilizzata per testare l'esistenza di una riga all'interno di un database quando
+     * si passa un unico paramentro che identifica quella riga all'interno della tabella
+     * @param $sql query da eseguire
+     */
+    public function exist($sql)
+    {
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (count($rows) == 1) return $rows[0];
+            else if (count($rows) > 1) return $rows;
+            $this->closeDbConnection();
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            die;
+            return null;
+        }
+    }
+
+
+    public static function getUpPath()
+    {
+        return static::$UpPath;
+    }
+
+    public function closeDbConnection() //funzione ausiliaria che pone il paramentro instance a null e che quindi
+    {                                   // chiude la connessione al DB
+        static::$instance = null;
+    }
+
+
+    /******************RICERCA*******************/
+
+    /**
+     * Effettua una ricerca sul database secondo i parametri selezionati.
+     * Tale metodo viene utilizzato per una ricerca avanzata da parte dell'utente.
+     * @param key la table da cui prelevare i dati
+     * @param value il valore per cui cercare i valori
+     * @param str il dato richiesto dall'utente
+     * @return array | NULL i risultati ottenuti dalla ricerca. Se la richiesta non ha match, ritorna NULL.
+     */
+    function cercaAv(string $key, string $value, string $str)
+    {
+        $sql = '';
+        $className = 'F' . $key;
+
+        if (class_exists($className)) {
+            $method = 'cerca' . $key . 'By' . $value;
+            if (method_exists($className, $method))
+                $sql = $className::$method();
+        }
+
+        if ($sql)
+            return $this->exeCerca('F' . $key, $value, $str, $sql);
+        else return NULL;
+    }
+
+    /**
+     * Funzione privata che prepara ed esegue la query.
+     * @return obj torna l'oggetto (Utente o Campagna) ricercato
+     */
+    private function exeCerca(string $className, string $value, string $str, string $sql)
+    {
+        try {
+            $stmt = $this->db->prepare($sql);       // creo PDOStatement
+            $stmt->bindValue(":" . lcfirst($value), $str, PDO::PARAM_STR); //si associa l'id al campo della query
+            $stmt->execute();   //viene eseguita la query
+            $stmt->setFetchMode(PDO::FETCH_ASSOC); // i risultati del db verranno salvati in un array con indici le colonne della table
+
+            $obj = NULL; // l'oggetto di ritorno viene definito come NULL
+
+            while ($row = $stmt->fetch()) {                                 // per ogni tupla restituita dal db...
+                $obj[] = FDatabase::createObjectFromRow($className, $row);  // si fa una istanza dell'oggetto
+            }
+            return $obj;
+        } catch (PDOException $e) {
+            return null;               // ritorna il parametro null se ci sono errori di vario tipo
+        }
+    }
+
+    /**
+     * Da una tupla ricevuta di una query istanzia l'oggetto corrispondente
+     * @param class il nome della classe
+     * @param row array la tupla restituita dal dbms
+     * @return obj l'oggetto risultato dell'elaborazione
+     */
+    private function createObjectFromRow(string $class, $row)
+    {
+        $obj = NULL; //oggetto che conterra' l'istanza dell'elaborazione
+
+        if (class_exists($class)) {
+            $obj = $class::createObjectFromRow($row);
+        }
+        return $obj;
+    }
+
+    /**
+     * Effettua una ricerca sul database secondo i parametri selezionati.
+     * Tale metodo viene utilizzato per una ricerca da parte dell'utente.
+     * @param cont parametro che indica quale gruppo di istruzioni eseguire
+     * @param str il dato richiesto dall'utente
+     * @return array | NULL i risultati ottenuti dalla ricerca. Se la richiesta non ha match, ritorna NULL.
+     */
+    function cerca($cont, string $str)
+    {
+        $sql = '';
+        if ($cont == 0) {
+            $method = 'cercaUtenteByUsername';
+            $sql = FUser::$method();
+            $className = 'Utente';
+            $value = 'Username';
+        } else if ($cont == 1) {
+            $method = 'cercaPrenotazioneByGiorno';
+            $sql = FBooking::$method();
+            $className = 'Prenotazione';
+            $value = 'Giorno';
+        } else if ($cont == 2) {
+            $method = 'cercaPrenotazioneByFascia';
+            $sql = FBooking::$method();
+            $className = 'Prenotazione';
+            $value = 'Fascia';
+        }
+        if ($sql)
+            return $this->exeCerca('F' . $className, $value, $str, $sql);
+        else return NULL;
+    }
+
+
+}
+?>
