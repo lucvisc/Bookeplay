ef37a047067c58b71280e7db47aabaed8c0d4df1
lucvisc <lucavisconti97@gmail.com>
2019-07-13 13:01:17 +0200

    Correzione della classe foundation FAddress

--- Foundation/FAddress.php
+++ Foundation/FAddress.php
@@ -7,9 +7,18 @@
  */
 require_once 'include.php';
 
-class FAddress extends FDatabase {
-
+class FAddress {
+    /**
+     * classe foundation
+     */
+    private static $class="FAddress";
+    /**
+     * tabella di riferimento
+     */
     private static $tables="Address";
+    /**
+     * valori della tabella
+     */
     private static $values="(:idAcc,:Comune,:Provincia,:CAP,:Via,:NumCivico)";
 
     public static function bind($stmt, EAddress $addr) {
@@ -21,13 +30,20 @@ class FAddress extends FDatabase {
         $stmt->bindValue(':Via', $addr->getVia(), PDO::PARAM_STR);
         $stmt->bindValue(':NumCivico', $addr->getNcivico(), PDO::PARAM_STR);
     }
+    /**
+     * questo metodo restituisce il nome della classe sul DB per la costruzione delle Query
+     * @return string $class classe della tabella di riferimento
+     */
+    public static function getClass(){
+        return self::$class;
+    }
 
     /**
      * questo metodo restituisce la stringa dei valori della tabella sul DB per la costruzione delle Query
      * @return string $values valori della tabella di riferimento
      */
     public static function getTables(){
-        return static::$tables;
+        return self::$tables;
     }
 
     /**
@@ -35,39 +51,98 @@ class FAddress extends FDatabase {
      * @return string $values valori della tabella di riferimento
      */
     public static function getValues(){
-        return static::$values;
+        return self::$values;
     }
 
-    public static function storeAddress($addr){
-        $sql="INSERT INTO ".static::getTables()." VALUES ".static::getValues();
+    /**
+     * Metodo che permette la store di un Utenteloggato
+     * @param $utente Utenteloggato da salvare
+     */
+    public static function store($address){
         $db=FDatabase::getInstance();
-        $id=$db->store($sql,"FAddress",$addr);
-        if($id) return $id;
-        else return null;
+        $id=$db->storeDB(static::getClass() ,$address);
     }
 
-    public static function loadAddressById($id){
-        $sql="SELECT * FROM ".static::getTables()." WHERE idAcc=".$id.";";
+    /**
+     * Permette la load sul db
+     * @param $id valore da confrontare per trovare l'oggetto
+     * @param $field campo nel quale effettuare la ricerca
+     * @return object $address Address
+     */
+    public static function loadByField($field, $id){
+        $address = null;
         $db=FDatabase::getInstance();
-        $result=$db->loadSingle($sql);
-        if($result!=null){
-            $addr=new EAddress($result['Comune'], $result['Provincia'],$result['CAP'],$result['Via'], $result['NumCivico']);
-            $addr->setId($result['idAcc']);
-            return $addr;
+        $result=$db->loadDB(static::getClass(), $field, $id);
+        $rows_number = $db->interestedRows(static::getClass(), $field, $id);    //funzione richiamata,presente in FDatabase
+        if(($result!=null) && ($rows_number == 1)) {        //:idAcc,:Comune,:Provincia,:CAP,:Via,:NumCivico
+            $address=new EAddress($result['idAcc'],$result['Comune'], $result['Provincia'], $result['CAP'],$result['Via'],$result['NumCivico']);
         }
-        else return null;
+        else {
+            if(($result!=null) && ($rows_number > 1)){
+                $address = array();
+                for($i=0; $i<count($result); $i++){
+                    $address[]=new EAddress($result[$i]['idAcc'],$result[$i]['Comune'], $result[$i]['Provincia'], $result[$i]['CAP'],$result[$i]['Via'],$result[$i]['NumCivico']);
+                }
+            }
+        }
+        return $address;
     }
 
+    /**
+     * Funzione che permette di verificare se esiste un indirizzo nel database
+     * @param  $id valore di cui verificare la presenza
+     * @param $field colonna su ci eseguire la verifica
+     * @return bool
+     */
+    public static function exist($field, $id){
+        $db=FDatabase::getInstance();
+        $result=$db->existDB(static::getClass(), $field, $id);    //funzione richiamata,presente in FDatabase
+        if($result!=null)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Metodo che aggiorna i campi di un Address
+     * @param $field campo nel quale si vuole modificare il valore
+     * @param $newvalue nuovo valore da assegnare
+     * @param $pk nome della colonna utilizzata per l'espressione "where" della query
+     * @param $id valore della primary key da usare come riferimento
+     * @return true se esiste il mezzo, altrimenti false
+     */
+    public static function update($field, $newvalue, $pk, $id){
+        $db=FDatabase::getInstance();
+        $result = $db->updateDB(static::getClass(), $field, $newvalue, $pk, $id);
+        if($result) return true;
+        else return false;
+    }
+
+    /**
+     * Permette la delete sul db in base all'id
+     * @param $field nome del campo della tabella nel quale ricercare il valore immesso
+     * @param $id valore del campo
+     * @return bool
+     */
+    public static function delete($field, $id){
+        $db=FDatabase::getInstance();
+        $result = $db->deleteDB(static::getClass(), $field, $id);   //funzione richiamata,presente in FDatabase
+        if($result) return true;
+        else return false;
+    }
+
+
+/*
     /**
      * Funzione ch permette la load dell comune
      * @param string $comune dell'user di riferimento
      * @return object $user
      */
-    public static function loadByComune($comune){
+ /*   public static function loadByComune($comune){
         $sql="SELECT * FROM ".static::getTables()." WHERE Comune='".$comune."';";
         $db=FDatabase::getInstance();
         $result=$db->loadSingle($sql);
-        if($result!=null){
+        if($result!=null){          //:idAcc,:Comune,:Provincia,:CAP,:Via,:NumCivico
             $addr=new EAddress($result['Comune'], $result['Provincia'],$result['CAP'],$result['Via'], $result['NumCivico']);
             $addr->setId($result['id']);
             return $addr;
@@ -80,7 +155,7 @@ class FAddress extends FDatabase {
      * @param int $id dell'account che fa riferimento ad un utente
      * @return object $addr
      */
-    public static function loadById($id){
+/*    public static function loadById($id){
         $sql="SELECT * FROM ".static::getTables()." WHERE id=".$id.";";
         $db=FDatabase::getInstance();
         $result=$db->loadSingle($sql);
@@ -109,7 +184,7 @@ class FAddress extends FDatabase {
      * @param int $id dell'utente che si vuole eliminare
      * @return bool
      */
-    public static function deleteAddress($id){
+ /*   public static function deleteAddress($id){
         $sql="DELETE FROM ".static::getTables()." WHERE idAcc=".$id.";";
         $db=FDatabase::getInstance();
         if($db->delete($sql)) return true;
@@ -124,27 +199,13 @@ class FAddress extends FDatabase {
      * @param string $newvalue nuovo valore da inserire nel DB
      * @return bool
      */
-    public static function UpdateAddress($id,$field,$newvalue){
+ /*   public static function UpdateAddress($id,$field,$newvalue){
         $sql="UPDATE ".static::getTables()." SET ".$field."='".$newvalue."' WHERE idAcc=".$id.";";
         $db=FDatabase::getInstance();
         if($db->update($sql)) return true;
         else return false;
     }
+ */
 
-    /**
-     * Istanzia l'oggetto Address dai risultati dlella query.
-     * @param string $row restituita dal dmbs
-     * @return EAddress|obj
-     */
-    public function createObjectFromRow($row) {
-        $indirizzo = new EAddress(); //costruisce l'istanza dell'oggetto
-        $indirizzo->setComune($row['Comune']);
-        $indirizzo->setProvincia($row['Provincia']);
-        $indirizzo->setCap($row['CAP']);
-        $indirizzo->setVia($row['Via']);
-        $indirizzo->setNcivicos($row['NumCivico']);
-
-        return $indirizzo;
-    }
 }
 ?>
