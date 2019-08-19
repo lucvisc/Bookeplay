76198737d3f6c75dfc9aff8ff426f1579ff21ed4
lucvisc <lucavisconti97@gmail.com>
2019-08-19 15:00:59 +0200

    classe Entity che gestisce l'immagine dell'user

---
+++ Entity/EMediaUser.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * La classe EMedia contiene tutti gli attributi e metodi base che riguardano l'immagine dell'user.
+ * Contiene i seguenti attributi (e i relativi metodi):
+ * -id: è un identificativo autoincrement, relativo ai media;
+ * -emailUser: è il corrispondere attributo per associare l'immagine all'user
+ * -filename: nome del media;
+ * -data: dati media.
+ * -type: MIME type dell'immagine
+ * @author Luca, Catriel
+ * @package Entity
+ */
+require_once "include.php";
+
+class EMedia
+{
+    /**
+     * id identificativo del media
+     * @AttributeType int
+     */
+    private $id;
+    /**
+     * email dell'utente
+     * @AttributeType string
+     */
+    private $emailUser;
+    /**
+     * nome del file media
+     * @AttributeType string
+     */
+    private $filename;
+    /**
+     * dati del media
+     * @AttributeType longblob
+     */
+    private $data;
+    /**
+     * tipo del media
+     * @AttributeType string
+     */
+    private $type;
+
+    //Dichiarazione del costruttore
+    public function __construct($fname, $emailUser)
+    {
+        $this->filename = $fname;
+        $this->emailUser = $emailUser;
+        $this->data = NULL;
+    }
+
+    /**
+     * @return int id media
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string email utente
+     */
+    public function getEmailUte()
+    {
+        return $this->emailute;
+    }
+
+    /**
+     * @return string nome media
+     */
+    public function getFname()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @return longblob dati media
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return string tipo media
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $id media
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param $email string email utente
+     */
+    public function setMailUte($email)
+    {
+        $this->emailUser = $email;
+    }
+
+    /**
+     * @param string $fname nome media
+     */
+    public function setFilename($fname)
+    {
+        $this->filename = $fname;
+    }
+
+    /**
+     * @param longblob $data informazioni media
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param string $tipo tipo media
+     */
+    public function setType($tipo)
+    {
+        $this->type = $tipo;
+    }
+
+    /**
+     * Verificano la corrispondenza con il valore in input con i requisiti richiesti
+     * @param $type valore inserito
+     * @return bool
+     */
+    public function valPic($type)
+    {
+        if ($type == "image/jpeg" || $type == "image/png")
+            return true;
+        else
+            return false;
+    }
+
+}
+?>
