8e42ace1ffd877b11f3f7bbf0b174fbcb6a24422
lucvisc <lucavisconti97@gmail.com>
2019-08-25 10:49:44 +0200

    aggiunta di script per utilità

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Utility/autoload.php';
+require_once 'ConfSmarty.php';
+require_once 'Installation.php';
+
+ if(Installation::verificaInstallazione()){ //si verifica se l'installazione è stata già fatta
+    $fcontroller=new CFrontController();
+    $fcontroller->run($_SERVER['REQUEST_URI']); //se è stata già fatta si invia al front controller la richiesta
+  }
+  // se l'installazione non è stata già fatta viene effettuata
+  else{
+            Installation::begin();
+  }
+
+?>
