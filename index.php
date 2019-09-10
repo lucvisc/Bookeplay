239f9943fb09742814639a8bf5c1076425d92ae0
lucvisc <lucavisconti97@gmail.com>
2019-09-10 15:30:14 +0200

    mod

--- index.php
+++ index.php
@@ -4,13 +4,7 @@ require_once 'ConfSmarty.php';
 require_once 'Installation.php';
 require_once 'include.php';
 
- if(Installation::verificaInstallazione()){ //si verifica se l'installazione è stata già fatta
     $fcontroller=new CFrontController();
     $fcontroller->run($_SERVER['REQUEST_URI']); //se è stata già fatta si invia al front controller la richiesta
-  }
-  // se l'installazione non è stata già fatta viene effettuata
-  else{
-            Installation::begin();
-  }
 
 ?>
